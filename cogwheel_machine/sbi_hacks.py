2df9c151952fc3a308772bad61ba892b64f296b2
Javier Roulet <jroulet@caltech.edu>
2025-02-11 14:31:41 -0800

    Fixed bug where there was unreachable code.

--- cogwheel_machine/sbi_hacks.py
+++ cogwheel_machine/sbi_hacks.py
@@ -62,26 +62,6 @@ class NPEFixedBatches(sbi.inference.NPE):
 
         return train_loader, val_loader
 
-
-def get_train_val_batch_inds(num_simulations, training_batch_size,
-                             validation_fraction):
-    """
-    Returns
-    -------
-    train_ind_batches, val_ind_batches: list of int arrays
-        Indices of the training and validation simulations, arranged in
-        batches.
-    """
-    num_batches = num_simulations // training_batch_size
-    batch_indices = np.split(np.arange(num_batches * training_batch_size),
-                             num_batches)
-
-    num_training_batches = int(
-        len(batch_indices) * (1-validation_fraction))
-    train_ind_batches = batch_indices[:num_training_batches]
-    val_ind_batches = batch_indices[num_training_batches:]
-    return train_ind_batches, val_ind_batches
-
     # Override ``train`` method to allow `lr_scheduler_kwargs`.
     # The code below is copied from sbi.inference.trainers.npe.npe_base
     # almost verbatim, the new lines w.r.t the sbi implementation have
@@ -318,6 +298,26 @@ def get_train_val_batch_inds(num_simulations, training_batch_size,
         self._neural_net.zero_grad(set_to_none=True)
 
         return deepcopy(self._neural_net)
+
+
+def get_train_val_batch_inds(num_simulations, training_batch_size,
+                             validation_fraction):
+    """
+    Returns
+    -------
+    train_ind_batches, val_ind_batches: list of int arrays
+        Indices of the training and validation simulations, arranged in
+        batches.
+    """
+    num_batches = num_simulations // training_batch_size
+    batch_indices = np.split(np.arange(num_batches * training_batch_size),
+                             num_batches)
+
+    num_training_batches = int(
+        len(batch_indices) * (1-validation_fraction))
+    train_ind_batches = batch_indices[:num_training_batches]
+    val_ind_batches = batch_indices[num_training_batches:]
+    return train_ind_batches, val_ind_batches
 
 
 class FixedBatchesDataLoader:
