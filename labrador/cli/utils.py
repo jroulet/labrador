c8a6d4970b9821623cc6502f0bd5c3e9ef8622fb
Javier Roulet <jroulet@caltech.edu>
2025-08-02 18:15:19 -0700

    Added commands `lab-setup-sbidir` and `lab-setup-unfolderdir`.

--- labrador/cli/utils.py
+++ labrador/cli/utils.py
@@ -1,5 +1,7 @@
 """
 Command-line interface to some functions in :py:mod:`~labrador.utils`.
+
+Entry points are defined in pyproject.toml under [project.scripts].
 """
 import argparse
 from pathlib import Path
@@ -31,3 +33,31 @@ def setup_rescalerdir_cli():
     args = parser.parse_args()
     rescalerdir = utils.setup_rescalerdir(**vars(args))
     print(f'rescalerdir: {rescalerdir}')
+
+
+def setup_sbidir_cli():
+    """CLI to :py:func:`~labrador.utils.setup_sbidir`."""
+    parser = argparse.ArgumentParser(
+        description='Create a new SBI directory under a given rescalerdir.')
+    parser.add_argument('rescalerdir', type=Path, help='Rescaler directory')
+    parser.add_argument(
+        '--prefix', type=str, default='sbi_',
+        help='Prefix for the sbi directory (default: sbi_)')
+
+    args = parser.parse_args()
+    sbidir = utils.setup_sbidir(**vars(args))
+    print(f'sbidir: {sbidir}')
+
+
+def setup_unfolderdir_cli():
+    """CLI to :py:func:`~labrador.utils.setup_unfolderdir`."""
+    parser = argparse.ArgumentParser(
+        description='Create a new unfolder directory in a given rescalerdir.')
+    parser.add_argument('rescalerdir', type=Path, help='Rescaler directory')
+    parser.add_argument(
+        '--prefix', type=str, default='unfolder_',
+        help='Prefix for the unfolder directory (default: unfolder_)')
+
+    args = parser.parse_args()
+    unfolderdir = utils.setup_unfolderdir(**vars(args))
+    print(f'unfolderdir: {unfolderdir}')
